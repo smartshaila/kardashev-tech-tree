6eb977e687f4289769e6a0fb6ed3c13244a0208b
smartshaila <smartshaila@gmail.com>
2018-05-31 22:48:41 -0500

    Added terraform line components (cost, tech line); Need to fix outline for pre-req based on pre-req count.

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const card = [
         title: 'Harvest Rover',
         level: 1,
         tech_line: 'harvest',
-        description: 'Gain a matching resource per fleet on an <b>colonized</b> tile during harvest',
+        description: 'Gain a matching resource per fleet on a <b>colonized</b> tile during harvest',
         flavor_text: 'You have created machines capable of harvesting additional resources',
         costs: [{resource_type: 'food', amount: 1}, {resource_type: 'matter', amount: 1}],
         prerequisites: [],
@@ -42,6 +42,45 @@ const card = [
         prerequisites: [{name:'Asteroid Mining', tech_line: 'harvest'}, {name:'Harvest Rover', tech_line: 'harvest'}],
         tech_action: false,
         flip_tech: false
+    },
+    {
+        id: 'scientist-terraform',
+        role: 'scientist',
+        title: 'Terraform',
+        level: 2,
+        tech_line: 'terraform',
+        description: 'Spend a resource to add a matching symbol or military symbol to your <b>colonized</b> tile',
+        flavor_text: 'You can modify the atmosphere and surface of your planets to make them more habitable',
+        costs: [{resource_type: 'food', amount: 1}, {resource_type: 'matter', amount: 1}, {resource_type: 'energy', amount: 1}, {resource_type: 'any', amount: 1}],
+        prerequisites: [],
+        tech_action: true,
+        flip_tech: false
+    },
+    {
+        id: 'scientist-antimatter',
+        role: 'scientist',
+        title: 'Antimatter',
+        level: 3,
+        tech_line: 'terraform',
+        description: 'Choose an <b>uncolonized</b> tile adjacent to one of your colonies. Destroy and flip it over. Collect all matching <b>resources.</b>',
+        flavor_text: 'You have developed a weapon strong enough to obliterate any planet within range',
+        costs: [{resource_type: 'food', amount: 1}, {resource_type: 'matter', amount: 1}, {resource_type: 'energy', amount: 1}, {resource_type: 'any', amount: 3}],
+        prerequisites: [{name:'Terraform', tech_line: 'terraform'}],
+        tech_action: true,
+        flip_tech: true
+    },
+    {
+        id: 'scientist-accretion-device',
+        role: 'scientist',
+        title: 'Accretion Device',
+        level: 3,
+        tech_line: 'terraform',
+        description: 'Spend a resource to add matching or military symbol to an adjacent <b>destroyed</b> tile',
+        flavor_text: 'Your scientists have developed a device capable of creating an entire habitable planet in the dead of space',
+        costs: [{resource_type: 'food', amount: 1}, {resource_type: 'matter', amount: 1}, {resource_type: 'energy', amount: 1}, {resource_type: 'any', amount: 3}],
+        prerequisites: [{name:'Terraform', tech_line: 'terraform'}],
+        tech_action: true,
+        flip_tech: true
     }
 ];
 
--- src/components/Prerequisite.js
+++ src/components/Prerequisite.js
@@ -5,8 +5,8 @@ class Prerequisite extends Component {
     render() {
         const prerequisiteClasses = classnames({
             prerequisite: true,
-            [this.props.tech_line]: true
-            // [`${this.props.name}-${this.props.tech_line}`]: true
+            [this.props.tech_line]: true,
+        // [`${this.props.name}-${this.props.tech_line}`]: true
         });
         return (
             <div className={prerequisiteClasses}>
